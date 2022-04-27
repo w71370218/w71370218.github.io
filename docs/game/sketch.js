c004cb85b30c7f68484836881cba629f48bb936e
w7147370218 <w7147370218@gmail.com>
2022-04-27 19:36:09 +0800

    202204271936

--- docs/game/sketch.js
+++ docs/game/sketch.js
@@ -148,25 +148,85 @@ let menulist = [];
 
 //let table = new Table();/////
 let maps = [];
+
+
+ //csv
+
+
 function preload() {
-  m = loadSound('assets/music/La_Campanella.mp3');
+  //m = loadSound('assets/music/La Campanella.mp3');
 }
 
 function setup() {
   createCanvas(screen_w, screen_h);
+  background(105, 105, 105);
+  textSize(300);
+  text("Loading...", 30, 30);
 
   let a_rgb = color(255, 79, 109);
   let d_rgb = color(60, 232, 227);
   let l_rgb = color(80, 250, 130);
   let r_rgb = color(255, 189, 79);
+
+  ypos = height * 0.25;
+  
+  music_index = 2;
+
+  status = 1;
 }
 
 function draw() {
-  if (!m.isPlaying()){
-    m.play();
+  if (status< 3 || status >5){
+    background(0);
+    if(background == 1){
+      fill(255);
+      for (let i = num-1; i > 0; i--) {
+        if (snow_y[i]<900){
+           ellipse(snow_x[i], snow_y[i], 10,10);
+         } else {
+           snow_x[i] = int(random(0,1200));
+           snow_y[i] = 0;
+           snow_v[i] = int(random(5,50));
+         }
+        snow_y[i] += snow_v[i];
+      }
+    }
   }
-  background(220);
-  ellipse(50,50,80,80);
+
+  if (status==1){
+    //frameRate(24);
+    
+    //file[music_index].stop();
+    
+    fill(255);
+    textSize(125);
+    text("Piano Tiles", 250, 300);
+    textSize(35);
+    text("By UR & Julie", 480, 400);
+    textSize(20);
+    text("Simple graphics, easy to play and everybody gets playing the piano!", 280, 480);
+    text("Fu Jen University Library and Information Science Department", 300, 850);
+    start.start(480,500,"Start");
+    setting.start(480,600,"Setting");
+    exit.start(480,700,"Exit");
+    
+    start.display();
+    setting.display();
+    exit.display();
+    
+    /*
+    if (mousePressed == true && mouseButton == LEFT && mouseX > 480 && mouseX < 720 && mouseY > 500 && mouseY < 580){
+      status=2;
+    }
+    if (mousePressed == true && mouseButton == LEFT && mouseX > 480 && mouseX < 720 && mouseY > 600 && mouseY < 680){
+      status=7;
+    }
+    
+    if (mousePressed == true && mouseButton == LEFT && mouseX > 480 && mouseX < 720 && mouseY > 700 && mouseY < 780){
+      exit();
+    }
+    */
+  }
 }
 function mousePressed() { 
   
